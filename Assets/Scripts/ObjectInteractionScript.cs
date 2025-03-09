d02f4f2cd142fcd0b3c30b28e1ab620619c7a661
Min Zay Ya <minzayya.augustinerichard@gmail.com>
2025-03-09 20:08:36 +0630

    Object Holding complete

--- Assets/Scripts/ObjectInteractionScript.cs
+++ Assets/Scripts/ObjectInteractionScript.cs
@@ -1,26 +1,110 @@
 using Unity.VisualScripting;
+using UnityEditor;
+using UnityEditor.Callbacks;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ObjectInteractionScript : MonoBehaviour
 {
-    private GameObject targetObject;
-    void OnTriggerEnter(Collider other)
+    GameObject targetObject;
+    public Transform heldObject;
+    public Transform holdPoint;
+    public static bool boxInHand = false;
+    void OnTriggerEnter(Collider other) // to detect objects with a box collider set under Camera object
     {
+        if (other.CompareTag("Player"))
+        {
+            return; // Exit the method without further processing to ignore player character from object targeting
+        }
+
         if(other.gameObject.CompareTag("Object"))
         {
             targetObject = other.gameObject;
             
         }
+
+        if(other.gameObject.CompareTag("Box"))
+        {
+            targetObject = other.gameObject;
+        }
         Debug.Log("Triggered by "+ other.gameObject.name);
         Debug.Log("Trigger");
     }
 
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        
+    }
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.E))
+        if(targetObject!=null)
+        {
+            
+            if(Input.GetMouseButton(0))
             {
-                Debug.Log("E");
-                Destroy(targetObject);
+                if(targetObject.tag == "Box")//to pick up boxes
+                {
+                    if(!boxInHand)
+                    {
+                        boxInHand = true;
+                        HoldObject();
+                           
+                    }
+                }
+                if(boxInHand)
+                {
+                    if(Input.GetKeyDown(KeyCode.Q))
+                    {
+                        Debug.Log("Q");
+                        RotateObject(Vector3.up);
+                    } 
+                }
+                
             }
+
+            else if(Input.GetMouseButtonUp(0))
+            {
+                if(targetObject.tag == "Box")//to pick up boxes
+                {
+                    if(boxInHand)
+                    {
+                        boxInHand = false;
+                        DropObject();
+                    }
+                }
+            }
+            
+
+            
+        
+        }
     }
+
+    void HoldObject()
+    {
+        targetObject.GetComponent<Rigidbody>().isKinematic = true;
+        targetObject.transform.position = holdPoint.position;
+        targetObject.transform.parent = holdPoint;
+
+        
+    }
+
+    void DropObject()
+    {
+        targetObject.GetComponent<Rigidbody>().isKinematic = false;
+        targetObject.transform.parent = null;
+    }
+
+    void RotateObject(Vector3 direction)
+    {
+        if(targetObject!=null)
+        {
+            targetObject.transform.Rotate(direction * 100f * Time.deltaTime);
+        }
+    }
+
+    
+
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -14,26 +14,29 @@ public class PlayerMovement : MonoBehaviour
     
     // Update is called once per frame
     void Update()
-    {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
+    {   
+        
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
+            Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+            if(direction.magnitude >= 0.1f)
+            {
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f); 
+                Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
 
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-        if(direction.magnitude >= 0.1f)
-        {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f); 
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                characterController.Move(moveDir.normalized * speed * Time.deltaTime);
+            }
+        
+        
+        
 
-            characterController.Move(moveDir.normalized * speed * Time.deltaTime);
-        }
-    }
-
-    private void FixedUpdate()
-    {
+        
         
     }
+
+   
 
 
 }
